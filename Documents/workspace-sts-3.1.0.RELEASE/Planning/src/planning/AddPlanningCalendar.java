ae39014435c000b2016860db5b7a6ccb638572ec
DAI Daocheng <gtonydai@gmail.com>
2013-05-28 17:58:19 +0200

    ajout planning  finish

--- Documents/workspace-sts-3.1.0.RELEASE/Planning/src/planning/AddPlanningCalendar.java
+++ Documents/workspace-sts-3.1.0.RELEASE/Planning/src/planning/AddPlanningCalendar.java
@@ -3,6 +3,7 @@ package planning;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import com.vaadin.addon.calendar.event.BasicEvent;
 import com.vaadin.addon.calendar.event.BasicEventProvider;
@@ -13,6 +14,8 @@ import com.vaadin.event.dd.DragAndDropEvent;
 import com.vaadin.event.dd.DropHandler;
 import com.vaadin.event.dd.acceptcriteria.AcceptAll;
 import com.vaadin.event.dd.acceptcriteria.AcceptCriterion;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.DragAndDropWrapper;
@@ -30,7 +33,9 @@ public class AddPlanningCalendar extends CustomComponent{
 	public TextField classNameInputField=new TextField("Le nom de cour:");	
 	public TextField classTimeInputField=new TextField("Duree(minute):");
 	public TextField classWeekInputField=new TextField("Semaine:");
-	public Label moveMe=new Label("Move me to calendar");
+	public Button moveMe=new Button("Move me to calendar");
+	public Button confirmAddPlanning=new Button("Valider");
+	public Button reset=new Button("Reset");
 	private MysqlConnection con;
 	public AddPlanningCalendar(String nom,String type) throws Exception {
 		setCompositionRoot(hl);
@@ -39,6 +44,7 @@ public class AddPlanningCalendar extends CustomComponent{
 		cal.setLastVisibleDayOfWeek(6);
 		cal.setFirstVisibleHourOfDay(8);
 		cal.setLastVisibleHourOfDay(20);
+		//cal.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
 		cal.setHeight("450px");
 		cal.setWidth("850px");
 		if(type=="eleve") {
@@ -72,18 +78,57 @@ public class AddPlanningCalendar extends CustomComponent{
 							(CalendarTargetDetails) event.getTargetDetails();
 					WrapperTransferable transferable =
 							(WrapperTransferable) event.getTransferable();
-				createEvent(details, transferable);
+				try {
+					createEvent(details, transferable);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				}
 
 				public AcceptCriterion getAcceptCriterion() {
 					return AcceptAll.get();
 				}});		
 		}
-		
+		classNameInputField.setRequired(true);
+		classNameInputField.setRequiredError("The Field may not be empty.");
+		classTimeInputField.setRequired(true);
+		classTimeInputField.setRequiredError("The Field may not be empty.");
+		classWeekInputField.setRequired(true);
+		classWeekInputField.setRequiredError("The Field may not be empty.");
+		moveMe.setVisible(false);		
+		confirmAddPlanning.addListener(new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				if(classNameInputField.getValue()!=""&&classTimeInputField.getValue()!=""&&classWeekInputField.getValue()!="" ){
+					//moveMe.setWidth("20px");
+					//moveMe.setHeight("30px");
+					
+				moveMe.setVisible(true);	}
+				else
+				{
+					getApplication().getMainWindow().showNotification(
+							"Les champs doivent pas null.");
+				}
+			}
+			
+		});
+		reset.addListener(new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				moveMe.setVisible(false);	
+				classNameInputField.setValue("");
+				classTimeInputField.setValue("");
+				classWeekInputField.setValue("");
+			}
+			
+		});
 		hl.addComponent(cal);				
 		vl.addComponent(classNameInputField);
 		vl.addComponent(classTimeInputField);
 		vl.addComponent(classWeekInputField);
+		vl.addComponent(confirmAddPlanning);
+		vl.addComponent(reset);
 		vl.addComponent(moveMe);
 		final DragAndDropWrapper classWrap = new DragAndDropWrapper(moveMe);
 		classWrap.setDragStartMode(DragStartMode.COMPONENT);
@@ -93,21 +138,34 @@ public class AddPlanningCalendar extends CustomComponent{
 		
 	}
 	protected void createEvent(CalendarTargetDetails details,
-			WrapperTransferable transferable) {
+			WrapperTransferable transferable) throws Exception {
 		Date dropTime = details.getDropTime();
 		java.util.Calendar timeCalendar = details.getTargetCalendar()
-				.getInternalCalendar();
-		timeCalendar.setTime(dropTime);	
-		
+				.getInternalCalendar();		
+		timeCalendar.setTime(dropTime);
+		timeCalendar.add(java.util.Calendar.HOUR, 32);//the original time is not the time i put on the calendar, there always exist 32 hours between them
+		int week=Integer.parseInt(classWeekInputField.getValue().toString());
+		for(int i=0;i<week;i++){		
+		Date startTime=timeCalendar.getTime();
 		timeCalendar.add(java.util.Calendar.MINUTE, Integer.parseInt(classTimeInputField.getValue().toString()));//
-		Date endTime = timeCalendar.getTime();			
+		Date endTime = timeCalendar.getTime();
+		java.sql.Timestamp sqlDate_begin = new java.sql.Timestamp(startTime.getTime());
+		//System.out.println(sqlDate_begin);
+		java.sql.Timestamp sqlDate_end = new java.sql.Timestamp(endTime.getTime());
+		//System.out.println(sqlDate_end);
+		MysqlConnection con = new MysqlConnection();
+		con.executeTable("INSERT INTO cours (Nom,Date_Debut,Date_Fin,Id_salle,id_matiere,id_professeur,id_groupe_cours) VALUES ('"
+						+ (String)classNameInputField.getValue()
+						+ "','"
+						+ sqlDate_begin
+						+ "','"
+						+ sqlDate_end + "',1,1,1,1)");
 		BasicEventProvider ep = (BasicEventProvider) details
-				.getTargetCalendar().getEventProvider();
-		//System.out.println((String)classNameInputField.getValue());
-		//System.out.println((String)classTimeInputField.getValue());
+				.getTargetCalendar().getEventProvider();		
 		ep.addEvent(new BasicEvent((String)classNameInputField.getValue(), "",
-				dropTime, endTime));
-		
+				startTime, endTime));
+		timeCalendar.add(java.util.Calendar.MINUTE, 24*7*60-Integer.parseInt(classTimeInputField.getValue().toString()));//For adding several weeks planning
+		}
 	}
 	
 
